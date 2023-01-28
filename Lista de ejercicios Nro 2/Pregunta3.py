18c86c11e4d646260a2ae6031ca5d0f5ca73b4c1
TatiL23 <122682528+TatiL23@users.noreply.github.com>
2023-01-28 07:55:39 -0500

    Update Pregunta3.py

---
+++ Lista de ejercicios Nro 2/Pregunta3.py
@@ -0,0 +1,9 @@
+One = int(input("Ingrese un número: "))
+Two = int(input("Ingrese el segundo número: "))
+
+if One>Two:
+    print("El mayor número es: ",One)
+elif Two>One:
+    print("El mayor número es: ",Two)
+else:
+    print("Los números son iguales")
